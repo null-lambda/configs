a17fb3f31c36e6470eb71e7268274246ea7816ea
null_lambda <5689817+null-lambda@users.noreply.github.com>
2023-07-02 03:17:49 +0900

    add some tex snippets

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -88,19 +88,22 @@ return {
     version = "1.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
     build = "make install_jsregexp",
     config = function() 
-      -- vim.cmd[[
-      -- " Use Tab to expand and jump through snippets
-      -- imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
-      -- smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
+      vim.cmd[[
+      " Use Tab to expand and jump through snippets
+      imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
+      smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
 
-      -- " Use Shift-Tab to jump backwards through snippets
-      -- imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
-      -- smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
-      -- ]]
+      " Use Shift-Tab to jump backwards through snippets
+      imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+      smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+      ]]
 
-      -- require("luasnip/loaders/from_snipmate").lazy_load({ paths = { "./snippets" }})
+      require("luasnip.loaders.from_lua").lazy_load({ paths = { "./snippets" } })
+      require("luasnip").config.set_config {
+        enable_autosnippets = true, 
+        store_selection_keys = "<Tab>", -- Trigger visual selection
+        update_events = 'TextChanged,TextChangedI', -- Update repeated nodes while typing
+      }
     end,
-    enable_autosnippets = true, 
-    store_selection_keys = "<Tab>", -- Trigger visual selection
   },
 }
